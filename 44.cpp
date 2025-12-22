5129e48e664ef5b15b65b2e7cdfb2aec299b5d8b
Harshit Yadav <official.harshit@outlook.com>
2025-12-22 21:45:12 +0530

    Refactor code structure for improved readability and maintainability

--- 44.cpp
+++ 44.cpp
@@ -63,3 +63,67 @@ void insertAtPosition(Node* &head, int position, int data){
     nodeToInsert->next = temp->next;
     temp->next = nodeToInsert;
 }
+
+void deleteNode(Node* &head, int position, Node* &tail){
+
+    //deleting first or start node
+    if(position == 1){
+
+        Node* temp = head;
+        head = head->next;
+        temp->next = NULL;
+        delete temp;
+    }else{
+
+        Node* curr = head;
+        Node* prev = NULL;
+        int count = 1;
+
+        while(count < position){
+            prev = curr;
+            curr = curr->next;
+            count++;
+        }
+
+        if(curr->next == NULL){
+            tail = prev;
+        }
+
+        if(curr == NULL){
+            cout << "Position is out of bounds" << endl;
+            return;
+        }
+
+        prev->next = curr->next;
+        curr->next = NULL;
+        delete curr;
+    }
+}
+
+void insertAtTail(Node* &tail, int data){
+    Node* temp = new Node(data);
+    tail->next = temp;
+    tail = temp;
+}
+
+int main(){
+
+    Node* node1 = new Node(10);
+
+    Node* head  = node1;
+    Node* tail = node1;
+    print(head);
+
+    insertAtTail(tail, 20);
+    print(head);
+
+    insertAtPosition(head, 1, 15);
+    print(head);
+
+    cout << "head: " << head->data << endl;
+    cout << "tail: " << tail->data << endl;
+
+    deleteNode(head, 3, tail);
+    print(tail);
+    print(head);
+}
